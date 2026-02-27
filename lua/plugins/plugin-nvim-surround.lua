e6365802ff676845047aa6c59a60d6a76366c76b
aq1u <aq1u@outlook.com>
2026-02-27 22:54:55 +0800

    fix: migrate nvim-surround to v4 API
    * Remove keymaps from setup function (v4 breaking change)
    * Use default keymaps provided by the plugin
    * Update surrounds configuration format

--- lua/plugins/plugin-nvim-surround.lua
+++ lua/plugins/plugin-nvim-surround.lua
@@ -3,56 +3,39 @@ return {
         "kylechui/nvim-surround",
         version = "*",
         event = "VeryLazy",
-        opts = {
-            keymaps = {
-                insert = "<C-g>s",
-                insert_line = "<C-g>S",
-                normal = "ys",
-                normal_cur = "yss",
-                normal_line = "yS",
-                normal_cur_line = "ySS",
-                visual = "S",
-                visual_line = "gS",
-                delete = "ds",
-                change = "cs",
-                change_line = "cS",
-            },
-            surrounds = {
-                ["("] = { add = { "(", ")" }, find = function()
-                    return M.get_selection({ motion = "a(" })
-                end, delete = "^(. ?)().-( ?.)()$" },
-                [")"] = { add = { "(", ")" }, find = function()
-                    return M.get_selection({ motion = "a(" })
-                end, delete = "^(. ?)().-( ?.)()$" },
-                ["{"] = { add = { "{", "}" }, find = function()
-                    return M.get_selection({ motion = "a{" })
-                end, delete = "^(. ?)().-( ?.)()$" },
-                ["}"] = { add = { "{", "}" }, find = function()
-                    return M.get_selection({ motion = "a{" })
-                end, delete = "^(. ?)().-( ?.)()$" },
-                ["<"] = { add = { "<", ">" }, find = function()
-                    return M.get_selection({ motion = "a<" })
-                end, delete = "^(. ?)().-( ?.)()$" },
-                [">"] = { add = { "<", ">" }, find = function()
-                    return M.get_selection({ motion = "a<" })
-                end, delete = "^(. ?)().-( ?.)()$" },
-                ["["] = { add = { "[", "]" }, find = function()
-                    return M.get_selection({ motion = "a[" })
-                end, delete = "^(. ?)().-( ?.)()$" },
-                ["]"] = { add = { "[", "]" }, find = function()
-                    return M.get_selection({ motion = "a[" })
-                end, delete = "^(. ?)().-( ?.)()$" },
-            },
-            highlight = {
-                duration = 0,
-            },
-            move_cursor = "begin",
-            indent_lines = function(start, stop)
-                local b = vim.bo
-                for i = start, stop do
-                    vim.cmd(i .. "left " .. b.shiftwidth)
-                end
-            end,
-        },
+        config = function()
+            require("nvim-surround").setup({
+                -- Keymaps are now set automatically by the plugin
+                -- See :h nvim-surround.usage for default mappings
+                --
+                -- Normal mode:
+                --   ys{motion}{char}  - Add surround
+                --   yss{char}         - Add surround to current line
+                --   ds{char}          - Delete surround
+                --   cs{old}{new}      - Change surround
+                --
+                -- Visual mode:
+                --   S{char}           - Add surround to selection
+                --
+                -- Insert mode:
+                --   <C-g>s{char}      - Add surround
+                --   <C-g>S{char}      - Add surround to current line
+
+                surrounds = {
+                    ["("] = { add = { "( ", " )" }, find = "(%(.-))", delete = "^(%( ?)().-( ?%))()$" },
+                    [")"] = { add = { "(", ")" }, find = "(%(.-))", delete = "^(%()().-(%))()$" },
+                    ["{"] = { add = { "{ ", " }" }, find = "({.-})", delete = "^({ ?)().-( ?})()$" },
+                    ["}"] = { add = { "{", "}" }, find = "({.-})", delete = "^({)().-(})()$" },
+                    ["<"] = { add = { "< ", " >" }, find = "(<.-)", delete = "^(< ?)().-( ?>)()$" },
+                    [">"] = { add = { "<", ">" }, find = "(<.-)", delete = "^(<)().-(>)()$" },
+                    ["["] = { add = { "[ ", " ]" }, find = "(%[.-%])", delete = "^(%[ ?)().-( ?%])()$" },
+                    ["]"] = { add = { "[", "]" }, find = "(%[.-%])", delete = "^(%[)().-(%])()$" },
+                },
+                highlight = {
+                    duration = 0,
+                },
+                move_cursor = "begin",
+            })
+        end,
     },
 }
