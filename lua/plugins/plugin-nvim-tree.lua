31410eeb9a385c64c08295d718dd101305c4c8c0
aq1u <aq1u@outlook.com>
2026-03-01 12:48:32 +0800

    fix: restore cursorline after closing nvim-tree
    * Add WinEnter autocmd to re-enable cursorline in normal windows
    * Prevents cursorline from disappearing after nvim-tree toggle

--- lua/plugins/plugin-nvim-tree.lua
+++ lua/plugins/plugin-nvim-tree.lua
@@ -3,12 +3,30 @@ return {
         "nvim-tree/nvim-tree.lua",
         version = "*",
         dependencies = {"nvim-tree/nvim-web-devicons"},
-        config = function() require("nvim-tree").setup {
-            view = {
-                side = "left"
-            }
-        }
-        end
-    }
-}
+        config = function()
+            require("nvim-tree").setup({
+                view = {
+                    side = "left",
+                },
+                on_attach = function(bufnr)
+                    local api = require("nvim-tree.api")
 
+                    -- Default mappings
+                    api.config.mappings.default_on_attach(bufnr)
+                end,
+            })
+
+            -- Fix cursorline after closing nvim-tree
+            local group = vim.api.nvim_create_augroup("NvimTreeCursorline", { clear = true })
+            vim.api.nvim_create_autocmd("WinEnter", {
+                group = group,
+                callback = function()
+                    -- Re-enable cursorline when entering normal windows
+                    if vim.bo.filetype ~= "NvimTree" then
+                        vim.opt_local.cursorline = true
+                    end
+                end,
+            })
+        end,
+    },
+}
