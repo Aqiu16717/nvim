6435c12a395d6401f7d0e54b8453b2f0965d25bd
aq1u <aq1u@outlook.com>
2026-03-22 16:27:50 +0800

    fix: simplify nvim-treesitter config
    - Remove all deprecated module references
    - Rely on Neovim 0.11+ built-in treesitter support
    - Plugin provides parsers only

--- lua/plugins/plugin-nvim-treesitter.lua
+++ lua/plugins/plugin-nvim-treesitter.lua
@@ -4,29 +4,7 @@ return {
         branch = "main",
         build = ":TSUpdate",
         lazy = false,
-        config = function()
-            -- Neovim 0.11+ has built-in treesitter support
-            -- This plugin mainly provides parsers and query files
-
-            -- Auto-install parsers
-            local ensure_installed = {
-                "lua", "vim", "vimdoc", "markdown", "markdown_inline",
-                "c", "cpp", "python", "go", "javascript", "typescript",
-                "json", "yaml", "toml", "bash", "regex", "query",
-            }
-
-            -- Check and install missing parsers
-            local function ensure_parsers()
-                local installed = require("nvim-treesitter.info").installed_parsers()
-                for _, lang in ipairs(ensure_installed) do
-                    if not vim.tbl_contains(installed, lang) then
-                        vim.cmd("TSInstall " .. lang)
-                    end
-                end
-            end
-
-            -- Run after startup
-            vim.defer_fn(ensure_parsers, 100)
-        end,
+        -- Neovim 0.11+ has built-in treesitter support
+        -- This plugin provides parsers only
     },
 }
